6f44740f82526b0989fe57f12ab4ca8add12aa81
Uma.jaishankar <Uma.jaishankar@ISCHLAPHP17146.IDC.Innovasolutions.int>
2019-10-10 15:07:55 +0530

    extra points

--- src/main/java/com/poc/clearinghouse/ClearingHouse.java
+++ src/main/java/com/poc/clearinghouse/ClearingHouse.java
@@ -8,7 +8,7 @@ public class ClearingHouse {
     public static void main(String[] args) {
     	
     	System.out.println("Clearing House");
-    	System.out.println("Adding Person");
+    	System.out.println("Adding Person with consideration of roles");
     }
 
 }
